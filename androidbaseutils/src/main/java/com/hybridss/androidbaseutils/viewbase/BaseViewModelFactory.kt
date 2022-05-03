e047e8852e044675036fe85b8aa66711d1363560
Jose Miguel Mercado <miguelooutn@gmail.com>
2022-05-02 19:46:56 -0500

    casteo desde clase

--- androidbaseutils/src/main/java/com/hybridss/androidbaseutils/viewbase/BaseViewModelFactory.kt
+++ androidbaseutils/src/main/java/com/hybridss/androidbaseutils/viewbase/BaseViewModelFactory.kt
@@ -5,4 +5,6 @@ import androidx.lifecycle.ViewModelProvider
 
 open class BaseViewModelFactory(private val inneriClass: Any) : ViewModelProvider.Factory {
     override fun <Any : ViewModel> create(modelClass: Class<Any>): Any = modelClass.cast(inneriClass)
+
+    fun getFactory() = this as ViewModelProvider.Factory
 }
