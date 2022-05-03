cb2990da5b8b2305c267fd923cb35933662d4d19
Jose Miguel Mercado <miguelooutn@gmail.com>
2022-05-02 19:17:38 -0500

    eliminacion de opcional

--- androidbaseutils/src/main/java/com/hybridss/androidbaseutils/viewbase/BaseViewModelFactory.kt
+++ androidbaseutils/src/main/java/com/hybridss/androidbaseutils/viewbase/BaseViewModelFactory.kt
@@ -4,5 +4,5 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 
 open class BaseViewModelFactory(private val inneriClass: Any) : ViewModelProvider.Factory {
-    override fun <T : ViewModel?> create(modelClass: Class<T>): T = inneriClass as T
+    override fun <Any : ViewModel> create(modelClass: Class<Any>): Any = inneriClass as Any
 }
