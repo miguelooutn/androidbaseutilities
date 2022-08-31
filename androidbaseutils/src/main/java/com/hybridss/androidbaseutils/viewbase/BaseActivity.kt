ee1266befb2967533c9b9114fec62a56af9dd709
Jose Miguel Mercado <miguelooutn@gmail.com>
2022-08-31 13:25:21 -0500

    Se corrige la carga del dialogo de loader (tronaba)

--- androidbaseutils/src/main/java/com/hybridss/androidbaseutils/viewbase/BaseActivity.kt
+++ androidbaseutils/src/main/java/com/hybridss/androidbaseutils/viewbase/BaseActivity.kt
@@ -22,7 +22,14 @@ import java.util.*
 open class BaseActivity : AppCompatActivity() {
 
 
-    private lateinit var progressDialog: Dialog
+    private val progressDialog: Dialog
+
+    init {
+        progressDialog = Dialog(this)
+        progressDialog.setContentView(R.layout.dialog_progress)
+        progressDialog.setCancelable(false)
+        progressDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+    }
 
     fun mostrarFragmentFade(
         fragmentIn: BaseFragment,
@@ -185,13 +192,6 @@ open class BaseActivity : AppCompatActivity() {
     }
 
     fun progress(it: Boolean) {
-        if (progressDialog == null) {
-            progressDialog = Dialog(this)
-            progressDialog.setContentView(R.layout.dialog_progress)
-            progressDialog.setCancelable(false)
-            progressDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
-        }
-
         if (progressDialog.isShowing && !it) {
             progressDialog.dismiss()
         } else{
